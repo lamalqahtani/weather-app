917d3526103771b3cd074821ff65ac582d0737a0
Lamyaa Q <lamiaa@Lamyaas-MacBook-Pro.local>
2022-08-10 11:52:15 +0300

    updateUI function added to get data from server and update the ui. all requirements now have been added. all what comes after this commit is enhancements

--- website/app.js
+++ website/app.js
@@ -4,17 +4,30 @@ const API_URL = "https://api.openweathermap.org/data/2.5/weather?";
 const API_KEY = "ac5815b64e47eca93a9671631b3f0150";
 let btn = document.getElementById('generate');
 let userFeeling = document.getElementById('feelings');
+let tempDiv = document.getElementById('temp');
+let dateDiv = document.getElementById('date');
+let contentDiv = document.getElementById('content');
 let zipCode = document.getElementById('zip');
 // zipCode = 94040; //only becouse there is no event listener yet
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
 
 // Event listener to add function to existing HTML DOM element
 btn.addEventListener('click', requestData);
 
 /* Function called by event listener */
 function requestData(){
-
     if(zipCode.value!== ''){
-        getWeatherData(API_URL,zipCode.value,API_KEY);
+        getWeatherData(API_URL,zipCode.value,API_KEY)
+        .then(data=>{
+            storeWeatherData('http://127.0.0.1:3000/data',
+            {temp: data.main.temp, date: newDate, resp: userFeeling.value });
+        })
+        .then(()=>getFromServerAndUpdateUI('http://127.0.0.1:3000/data'))
+        .catch(error=> alert(error));
     }else{
         alert('insert a valid zip code.')
     }
@@ -26,7 +39,8 @@ async function getWeatherData(baseUrl='',userZipCode='',appid=''){
     try{
         let data = await response.json();
         console.log(data);
-        storeWeatherData('http://127.0.0.1:3000/data',{temp: data.main.temp, date: data.dt, resp: userFeeling.value });
+        return data;
+        // storeWeatherData('http://127.0.0.1:3000/data',{temp: data.main.temp, date: newDate, resp: userFeeling.value });
     }catch(error){
         console.log(error);
     }
@@ -49,17 +63,33 @@ async function storeWeatherData(url = '',data ={}){
     });
 
     try{
-        let data = await response.json();
+        // let data = await response.json();
         console.log('finished post request');
+        // getFromServer();
     }catch(error){
         console.log(error);
     }
 }
 
 
-// storeWeatherData('http://127.0.0.1:3000/data',{temp: 10, date: new Date(), resp: "nothing" });
+/* Function to GET Project Data */
 
-
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// url = 'http://127.0.0.1:3000/data'
+async function getFromServerAndUpdateUI(url = ''){
+    let response = await fetch(url,{
+        method:'GET',
+        credentials: 'same-origin',
+        headers:{
+            'content-type': 'application/json'
+        }
+    });
+    try{
+        let data = await response.json();
+        console.log(data);
+        tempDiv.textContent = data.temp;
+        dateDiv.textContent = data.date;
+        contentDiv.textContent = data.response;
+    }catch(error){
+        console.log(error);
+    }
+}
